abc800c2b474b4a1a231e53d045f69e3b57664f9
ImNotStable <jeremiahhughes06@gmail.com>
2024-05-03 01:58:20 -0500

    Complete Plugin

--- src/main/java/com/imnotstable/coolteleport/CoolTeleport.java
+++ src/main/java/com/imnotstable/coolteleport/CoolTeleport.java
@@ -1,14 +1,30 @@
 package com.imnotstable.coolteleport;
 
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIBukkitConfig;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CoolTeleport extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-    }
-
-    @Override
-    public void onDisable() {
-    }
+  
+  @Getter
+  private static CoolTeleport instance;
+  
+  @Override
+  public void onLoad() {
+    CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
+  }
+  
+  @Override
+  public void onEnable() {
+    instance = this;
+    CommandAPI.onEnable();
+    CoolTeleportCommand.getCoolTeleportCommand().register();
+  }
+  
+  @Override
+  public void onDisable() {
+    CommandAPI.onDisable();
+  }
+  
 }
--- src/main/java/com/imnotstable/coolteleport/CoolTeleportCommand.java
+++ src/main/java/com/imnotstable/coolteleport/CoolTeleportCommand.java
@@ -1,7 +1,39 @@
 package com.imnotstable.coolteleport;
 
+import dev.jorel.commandapi.CommandTree;
+import dev.jorel.commandapi.arguments.LocationArgument;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 public class CoolTeleportCommand {
   
-  private static final CommandA
+  @Getter
+  private static final CommandTree coolTeleportCommand = new CommandTree("coolteleport")
+    .withAliases("cooltp", "ctp")
+    .then(new PlayerArgument("target")
+      .executesPlayer((player, args) -> {
+        if (args.get("target") instanceof Player target)
+          teleport(player, target.getLocation());
+      }))
+    .then(new LocationArgument("location")
+      .executesPlayer((player, args) -> {
+        if (args.get("location") instanceof Location location)
+          teleport(player, location);
+      }));
+  
+  private static void teleport(Player teleporter, Location location) {
+    if (teleporter.hasPermission("coolteleport.instant")) {
+      teleporter.teleportAsync(location);
+      return;
+    }
+    if (teleporter.hasPermission("coolteleport.short")) {
+      Bukkit.getScheduler().runTaskLater(CoolTeleport.getInstance(), () -> teleporter.teleportAsync(location), 20L);
+      return;
+    }
+    Bukkit.getScheduler().runTaskLater(CoolTeleport.getInstance(), () -> teleporter.teleportAsync(location), 100L);
+  }
   
 }
